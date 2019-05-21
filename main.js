c8299d101f5f17b77db394ff781bbb71c20d678a
daviderlich <33066205+daviderlich@users.noreply.github.com>
2019-05-21 09:54:31 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let x;
+/*let x;
 let y;
 
 let xspeed;
@@ -53,4 +53,16 @@ function draw() {
     y = 0;
     pickColor();
   }
+}*/
+function setup() {
+  createCanvas(500, 500, WEBGL);
+  fill(random(255), random(255), random(255))
+}
+
+function draw() {
+  background(200);
+  rotateX(frameCount * 0.11);
+  rotateY(frameCount * 0.11);
+  noStroke();
+  sphere(100, 10, 90)
 }
