38e5635b97e2c85d6f86dd62a8448f6231568023
daviderlich <33066205+daviderlich@users.noreply.github.com>
2019-06-07 10:00:39 -0700

    Update Snake.js

--- Snake.js
+++ Snake.js
@@ -16,7 +16,7 @@ let h = 180;
 
 function setup() {
   scoreElem = createDiv('Score = 0');
-  scoreElem.position(20, 1000);
+  scoreElem.position(20, 500);
   scoreElem.id = 'score';
   scoreElem.style('color', 'white');
   createCanvas(700, 500);
