7f326848b0324f762e0b97deae8a7c2d2e78f403
daviderlich <33066205+daviderlich@users.noreply.github.com>
2019-06-10 11:17:15 -0700

    Update Snake.js

--- Snake.js
+++ Snake.js
@@ -1,137 +1,57 @@
-var numSegments = 10;
-var direction = 'right';
-
-var xStart = 0; 
-var yStart = 250; 
-var diff = 10;
-
-var xCor = [];
-var yCor = [];
-
-var xFruit = 0;
-var yFruit = 0;
-var scoreElem;
-let c = 0
-let h = 180;
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
 
 function setup() {
-  scoreElem = createDiv('Score = 0');
-  scoreElem.position(20, 200);
-  scoreElem.id = 'score';
-  scoreElem.style('color', 'white');
-  createCanvas(700, 500);
-  strokeWeight(10);
-  updateFruitCoordinates();
-
-  for (var i = 0; i < numSegments; i++) {
-    xCor.push(xStart + (i * diff));
-    yCor.push(yStart);
-  }
+  createCanvas(400, 400);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5);
+  snake = new Snake();
+  foodLocation();
 }
 
-function draw() {
-    if (c>=360) c=0; else; c++;
-  stroke(c,100,100);
-      if (h>=360) h=0; else; h++;
-  background(h,100,70,);
-  for (var i = 0; i < numSegments - 1; i++) {
-    line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
-  frameRate(18)
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
   
-  
-  }
-  updateSnakeCoordinates();
-  checkGameStatus();
-  checkForFruit();
-}
-
-function updateSnakeCoordinates() {
-
-  for (var i = 0; i < numSegments - 1; i++) {
-    xCor[i] = xCor[i + 1];
-    yCor[i] = yCor[i + 1];
-  }
-  switch (direction) {
-    case 'right':
-      xCor[numSegments - 1] = xCor[numSegments - 2] + diff;
-      yCor[numSegments - 1] = yCor[numSegments - 2];
-      break;
-    case 'up':
-      xCor[numSegments - 1] = xCor[numSegments - 2];
-      yCor[numSegments - 1] = yCor[numSegments - 2] - diff;
-      break;
-    case 'left':
-      xCor[numSegments - 1] = xCor[numSegments - 2] - diff;
-      yCor[numSegments - 1] = yCor[numSegments - 2];
-      break;
-    case 'down':
-      xCor[numSegments - 1] = xCor[numSegments - 2];
-      yCor[numSegments - 1] = yCor[numSegments - 2] + diff;
-      break;
-  }
-}
-
-function checkGameStatus() {
-  if (xCor[xCor.length - 1] > width ||
-      xCor[xCor.length - 1] < 0 ||
-      yCor[yCor.length - 1] > height ||
-      yCor[yCor.length - 1] < 0 ||
-      checkSnakeCollision()) {
-    noLoop();
-    var scoreVal = parseInt(scoreElem.html().substring(8));
-    scoreElem.html('Game ended! Your score was : ' + scoreVal);
-  }
-}
-
-function checkSnakeCollision() {
-  var snakeHeadX = xCor[xCor.length - 1];
-  var snakeHeadY = yCor[yCor.length - 1];
-  for (var i = 0; i < xCor.length - 1; i++) {
-    if (xCor[i] === snakeHeadX && yCor[i] === snakeHeadY) {
-      return true;
-    }
-  }
-}
-
-function checkForFruit() {
-  point(xFruit, yFruit);
-  if (xCor[xCor.length - 1] === xFruit && yCor[yCor.length - 1] === yFruit) {
-    var prevScore = parseInt(scoreElem.html().substring(8));
-    scoreElem.html('Score = ' + (prevScore + 1));
-    xCor.unshift(xCor[0]);
-    yCor.unshift(yCor[0]);
-    numSegments++;
-    updateFruitCoordinates();
-  }
-}
-
-function updateFruitCoordinates() {
-
-  xFruit = floor(random(10, (width - 100) / 10)) * 10;
-  yFruit = floor(random(10, (height - 100) / 10)) * 10;
 }
 
 function keyPressed() {
-  switch (keyCode) {
-    case 65:
-      if (direction != 'right') {
-        direction = 'left';
-      }
-      break;
-    case 68:
-      if (direction != 'left') {
-        direction = 'right';
-      }
-      break;
-    case 87:
-      if (direction != 'down') {
-        direction = 'up';
-      }
-      break;
-    case 83:
-      if (direction != 'up') {
-        direction = 'down';
-      }
-      break;
+  if(keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+  	snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+  	snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+  	snake.setDir(0, -1);
+  } else if (key == ' ') {
+  	snake.grow();
   }
+
+}
+
+function draw() {
+  scale(rez);
+  background(220);
+  if (snake.eat(food)) {
+     foodLocation();
+  }
+  snake.update();
+  snake.show();
+  
+  
+  if (snake.endGame()) {
+  	print("END GAME");
+    background(255, 0, 0);
+    noLoop();
+  }
+  
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
 }
